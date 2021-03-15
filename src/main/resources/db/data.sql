77240c18633a3947935bc5ae575ca3b58373d46e
marineLi <13820203466@163.com>
2021-03-15 08:28:29 +0800

    在线订阅课程测试代码

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,7 +2,7 @@ INSERT INTO `role`(`id`, `createdate`, `name`) VALUES (1, NULL, 'admin');
 INSERT INTO `role`(`id`, `createdate`, `name`) VALUES (2, NULL, 'teacher');
 INSERT INTO `role`(`id`, `createdate`, `name`) VALUES (3, NULL, 'student');
 
-INSERT INTO `user`(`id`, `createdate`, `email`, `name`, `password`, `sex`,  `grade`) VALUES (1, '2021-03-12 09:59:48', 'admin@111.com', 'admin', '0709e06f5eb1347c3894eaea91919226', 1, NULL);
+INSERT INTO `user`(`id`, `createdate`, `email`, `name`, `password`, `sex`,  `grade`) VALUES (1, '2021-03-12 09:59:48', 'admin@111.com', 'Admin', '4b623141ef1064483d8fe6c165042a6a', 1, NULL);
 INSERT INTO `user`(`id`, `createdate`, `email`, `name`, `password`, `sex`,  `grade`) VALUES (2, '2021-03-12 09:59:48', 'aa@test.com', '李老师', '0709e06f5eb1347c3894eaea91919226', 1,  NULL);
 INSERT INTO `user`(`id`, `createdate`, `email`, `name`, `password`, `sex`,  `grade`) VALUES (3, '2021-03-12 10:33:13', 'b@sdfsdf.com', '同学1', '0709e06f5eb1347c3894eaea91919226', 0,  NULL);
 INSERT INTO `user`(`id`, `createdate`, `email`, `name`, `password`, `sex`,  `grade`) VALUES (4, '2021-03-12 17:20:21', 'b@sdfsdf.com', '同学2', '0709e06f5eb1347c3894eaea91919226', 0,  NULL);
